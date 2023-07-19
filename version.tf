b5431a230c2caa262589efa28ea335bbdb4f3bac
Lvov <lvovas@gmail.comm>
2023-07-19 15:18:31 +0300

    check

--- version.tf
+++ version.tf
@@ -13,8 +13,6 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    skip_get_ec2_platforms      = true
     
     dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g8np9vscpqf0c15ej1/etngu6933nkbuqdd0n9f"
     dynamodb_table = "state-lock-table"
