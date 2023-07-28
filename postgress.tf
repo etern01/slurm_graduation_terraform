ff7b3f6194eba1d2f2a733518e22d0038c8fe7af
Lvov <lvovas@gmail.comm>
2023-07-28 15:22:20 +0300

    fix rubby

--- postgress.tf
+++ postgress.tf
@@ -1,11 +1,14 @@
 resource "yandex_mdb_postgresql_cluster" "this" {
   name                = "yelb-db"
-  environment         = "PRODUCTION"
+  environment         = "PRESTABLE"
   network_id          = yandex_vpc_network.this.id
   deletion_protection = false
 
   config {
-    version = 12
+    version = 11
+    access {
+      web_sql = true
+    }
     resources {
       resource_preset_id = "s2.micro"
       disk_type_id       = "network-ssd"
@@ -17,7 +20,9 @@ resource "yandex_mdb_postgresql_cluster" "this" {
     zone      = "ru-central1-a"
     name      = "mypg-host-a"
     subnet_id = yandex_vpc_subnet.this["ru-central1-a"].id
+    assign_public_ip = true
   }
+
 }
 
 
